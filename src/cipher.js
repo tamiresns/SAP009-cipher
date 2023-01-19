ddc25b7eb550d64abbcb0fe8f46cf90c7cdf3fc4
tamiresns <tamiresmn@live.com>
2023-01-19 18:50:56 -0300

    início do JS

--- src/cipher.js
+++ src/cipher.js
@@ -2,8 +2,22 @@ const cipher = {
   // ...
 };
 
-function encode(offset, string) {}
+let codigoDaLetra;
+let codigoDaPrimeiraLetra;
+let numeroDeslocamento;
+let tamanhoAlfabeto = 25;
 
-function decode(offset, string) {}
+
+function encode(numeroDeslocamento, string) {
+  let cifrar = ((codigoDaLetra - codigoDaPrimeiraLetra + numeroDeslocamento) % tamanhoAlfabeto) + codigoDaPrimeiraLetra;
+
+  return cifrar;
+}
+
+function decode(numeroDeslocamento, string) {
+  let deCifrar = ((codigoDaLetra - codigoDaPrimeiraLetra - numeroDeslocamento) % tamanhoAlfabeto) + codigoDaPrimeiraLetra;
+
+  return deCifrar;
+}
 
 export default cipher;
