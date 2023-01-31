c0599401aa360c719ce19717ce1a59b52b240eb4
tamiresns <tamiresmn@live.com>
2023-01-31 17:12:32 -0300

    Finalizado e testado

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,54 @@
-const cipher = {
- //..
-};
-
 let codigoDaPrimeiraLetraMaiuscula = 65;
+let codigoDaUltimaLetraMaiuscula = 90;
 const tamanhoAlfabeto = 26;
 
 function descobreASCInicial(cifraRecebida) {
 
-  if(cifraRecebida == cifraRecebida.toUpperCase()) {
-      return 65;
+  if(cifraRecebida === cifraRecebida.toUpperCase()) {
+    return 65;
   }
   else {
-      return 97; //Retorna ASC code do a minusculo
+    return 97; //Retorna ASC code do a minusculo
   }
- }
+}
+function descobreASCFinal(cifraRecebida) {
+
+  if(cifraRecebida === cifraRecebida.toUpperCase()) {
+    return 90;
+  }
+  else {
+    return 122; //Retorna ASC code do a minusculo
+  }
+}
 
 
 function encode(numeroDeslocamento, cifraRecebida) {
   let resultado = "";
   
   for(let i = 0; i < cifraRecebida.length; i++){
-    let codigoDaLetra = cifraRecebida.charCodeAt(i);
-    console.log("codigo 1 pletra: " + codigoDaLetra, "letra: "+cifraRecebida[i]);
+    const codigoDaLetra = cifraRecebida.charCodeAt(i);
     codigoDaPrimeiraLetraMaiuscula = descobreASCInicial(cifraRecebida[i]);
 
-    let cifrar = ((codigoDaLetra - codigoDaPrimeiraLetraMaiuscula + numeroDeslocamento) % tamanhoAlfabeto) + codigoDaPrimeiraLetraMaiuscula;
+    const cifrar = ((codigoDaLetra - codigoDaPrimeiraLetraMaiuscula + numeroDeslocamento) % tamanhoAlfabeto) + codigoDaPrimeiraLetraMaiuscula;
     resultado = resultado + String.fromCharCode(cifrar);
 
-    console.log(cifrar);
   }
   return resultado;
 
 }
 
 function decode(numeroDeslocamento, cifraRecebida) {
-  let deCifrar = ((codigoDaLetra - codigoDaPrimeiraLetra - numeroDeslocamento) % tamanhoAlfabeto) + codigoDaPrimeiraLetra;
+  let resultado = "";
 
-  return deCifrar;
+  for(let i = 0; i < cifraRecebida.length; i++){
+    const codigoDaLetra = cifraRecebida.charCodeAt(i);
+    codigoDaUltimaLetraMaiuscula = descobreASCFinal(cifraRecebida[i]);
+
+    const deCifrar = ((codigoDaLetra - codigoDaUltimaLetraMaiuscula - numeroDeslocamento) % tamanhoAlfabeto) + codigoDaUltimaLetraMaiuscula;
+    resultado = resultado + String.fromCharCode(deCifrar);
+
+  }  
+  return resultado;
 }
 
 //export default cipher;
--- src/index.js
+++ src/index.js
@@ -6,27 +6,30 @@ import {encode, decode} from './cipher.js';
 //2 cifrar
 //3 mostrar o valor cifrado
 
-let botaoCifra = document.getElementById("btnCifra");
+const botaoCifra = document.getElementById("btnCifra");
 botaoCifra.addEventListener("click", cifra);
 
 function cifra() {
-    let deslocamento = parseInt(document.getElementById("inputDeslocamento").value);
-    let cifraRecebida = document.getElementById("inputRecebe").value;
+  const deslocamento = parseInt(document.getElementById("inputDeslocamento").value);
+  const cifraRecebida = document.getElementById("inputRecebe").value;
 
-    let palavraCifrada = encode(deslocamento, cifraRecebida);
-    console.log(palavraCifrada)
+  const palavraCifrada = encode(deslocamento, cifraRecebida);
     
-    document.getElementById("inputEntrega").value = palavraCifrada;
+  document.getElementById("inputEntrega").value = palavraCifrada;
 
 }
 
-function deCifra() {
-    let deslocamento = parseInt(document.getElementById("inputDeslocamento").value);
-    let cifraEntrega = document.getElementById("inputEntrega").value;
+const botaoDecifra = document.getElementById("btnDecifra");
+botaoDecifra.addEventListener("click", deCifra);
 
-    let palavraDecifrada = decode(deslocamento, cifraEntrega);
-    
-    document.getElementById("inputRecebe").value = palavraDecifrada;
+
+function deCifra() {
+  const deslocamento = parseInt(document.getElementById("inputDeslocamento").value);
+  const cifraEntrega = document.getElementById("inputEntrega").value;
+
+  const palavraDecifrada = decode(deslocamento, cifraEntrega);
+
+  document.getElementById("inputRecebe").value = palavraDecifrada;
 
 }
 
